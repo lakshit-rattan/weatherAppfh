45997d3c0d3f86ce73d92e3899d285209e89d8eb
Lakshit Rattan <lakshitrrattan@gmail.com>
2024-09-23 03:34:39 +0530

    ADD: project demo on iOS, Hourly card responsive UI for different length devices

--- src/components/HourlyForecastCard/HourlyForecastCard.tsx
+++ src/components/HourlyForecastCard/HourlyForecastCard.tsx
@@ -22,27 +22,20 @@ export default function HourlyForecastCard({
 	hourData,
 }: HourlyForecastCardProps) {
 	return (
-		<View
-			style={{ minWidth: 163, maxWidth: 193 }}
-			className="bg-gray-800 p-4 justify-center rounded-lg mx-1"
-		>
-			<Text
-				style={{ alignSelf: 'center' }}
-				className="text-white text-2xl mb-1 font-bold"
-			>
+		<View className="bg-gray-800 p-4 justify-center rounded-lg mx-2 w-44 min-h-50">
+			<Text className="text-white text-2xl mb-2 font-bold text-center">
 				{hourData.time.split(' ')[1]}
 			</Text>
 			<Image
 				source={{ uri: `https:${hourData?.condition?.icon}` }}
-				className="w-28 h-28"
-				style={{ alignSelf: 'center' }}
+				className="w-20 h-20 mx-auto"
 				resizeMode="contain"
 			/>
-			<Text className="text-white  mb-1">• Temp: {hourData.temp_c}°</Text>
-			<Text className="text-white  mb-1">
+			<Text className="text-white mb-1">• Temp: {hourData.temp_c}°</Text>
+			<Text className="text-white mb-1">
 				• Condition: {hourData?.condition?.text}
 			</Text>
-			<Text className="text-white  mb-1">• Wind: {hourData?.wind_kph} kph</Text>
+			<Text className="text-white mb-1">• Wind: {hourData?.wind_kph} kph</Text>
 			<Text className="text-white mb-1">
 				• Precipitation: {hourData?.precip_mm} mm
 			</Text>
